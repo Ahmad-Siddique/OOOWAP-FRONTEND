41afaa5ee11220fdc424194ed15446a64e8552a8
cyhammad <hammad727272@gmail.com>
2024-09-24 01:49:20 +0500

    hero fix

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,14 +1,14 @@
 import { auth } from "@/auth";
 import Carousal from "@/components/landing-page/Carousal";
 import Featured from "@/components/landing-page/Featured";
-import HERO from "@/components/landing-page/HERO";
+import Hero from "@/components/landing-page/Hero";
 import Tiers from "@/components/landing-page/Tiers";
 
 export default async function Home() {
   const session = await auth();
   return (
     <>
-      <HERO />
+      <Hero />
       <Featured loginInfo={session} />
       <Carousal />
       <Tiers />
